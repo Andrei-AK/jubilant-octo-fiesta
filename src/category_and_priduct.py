717fd95434cdea2d5ac5bb36b1524230e54729bd
Andrei <ggread123.rezerv@gmail.com>
2025-04-27 23:20:18 +0700

    add getter price, setter price, getter products, add_product, new_product

--- src/category_and_priduct.py
+++ src/category_and_priduct.py
@@ -2,24 +2,35 @@ class Product:
 
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
-
     @classmethod
-    def new_product(cls,dict_product_data: dict):
+    def new_product(cls, dict_product_data: dict):
         return cls(
-            name=dict_product_data['name'],
-            description=dict_product_data['description'],
-            price=dict_product_data['price'],
-            quantity=dict_product_data['quantity']
+            name=dict_product_data["name"],
+            description=dict_product_data["description"],
+            price=dict_product_data["price"],
+            quantity=dict_product_data["quantity"],
         )
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value > 0:
+            self.__price = value
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
+
 
 class Category:
 
@@ -38,11 +49,9 @@ class Category:
         Category.total_categories += 1
         Category.total_products += len(self.__products)
 
-
     def add_product(self, product):
         self.__products.append(product)
         Category.total_products += 1
-
 
     @property
     def products(self):

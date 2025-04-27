0231727f17dc1b1cf5105193f7081abee2cd13b1
Andrei <ggread123.rezerv@gmail.com>
2025-04-27 12:54:16 +0700

    add new_product, add_product products maked private (__products)

--- src/category_and_priduct.py
+++ src/category_and_priduct.py
@@ -12,11 +12,20 @@ class Product:
         self.quantity = quantity
 
 
+    @classmethod
+    def new_product(cls,dict_product_data: dict):
+        return cls(
+            name=dict_product_data['name'],
+            description=dict_product_data['description'],
+            price=dict_product_data['price'],
+            quantity=dict_product_data['quantity']
+        )
+
 class Category:
 
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product]
 
     total_categories = 0
     total_products = 0
@@ -24,7 +33,19 @@ class Category:
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
 
         Category.total_categories += 1
-        Category.total_products += len(self.products)
+        Category.total_products += len(self.__products)
+
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.total_products += 1
+
+
+    @property
+    def products(self):
+        for product in self.__products:
+            print(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
+        return self.__products
